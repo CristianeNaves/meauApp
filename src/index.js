0d45552ff7ff2012724300795eb874a53d531b1b
Vitor Dellamora <vitormd96@gmail.com>
2021-03-14 16:00:24 -0300

    Navigator inicial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView} from 'react-native';
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <SafeAreaView>
+    <NavigationContainer>
+      {/* <View>
+        <Text>Hello</Text>
+      </View> */}
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </SafeAreaView>
   );
 }
