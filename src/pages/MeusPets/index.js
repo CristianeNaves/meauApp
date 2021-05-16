bda6332b11dacbf7ddc23f8e3ba3204e10bf1c43
Cristiane Naves <cristianenavescardoso09@gmail.com>
2021-05-16 17:27:43 -0300

    arrumando meus pets

--- src/pages/MeusPets/index.js
+++ src/pages/MeusPets/index.js
@@ -3,66 +3,38 @@ import {View, Text} from 'react-native';
 import AuthContext from '../../contexts/auth';
 import {getAll} from '../../services/pet';
 import {ListItem} from 'react-native-elements';
-import { LargeImage } from '../../components/Image';
+import {LargeImage} from '../../components/Image';
 import {Button, Card} from 'react-native-paper';
 import styles from './style';
-
-
 import storage from '@react-native-firebase/storage';
-import { Alert } from 'react-native';
-
-const PetItem = ({navigation, pet}) => {
-
-  const [petPhoto, setPetPhoto] = useState({uri: "https://i.pinimg.com/originals/18/82/e0/1882e07aecdf7a3286a5013cdad5d0c0.png"});
-
-  try {
-    const image = storage().ref().child(pet.photoFile);
-    // const image = images.child('image1');
-    image.getDownloadURL().then((url) => { 
-      setPetPhoto({ uri: url })
-      // console.log(petPhoto);
-    })
-    .catch(error => {
-      console.log("Não foi possível resgatar foto de animal.");
-    });
-  } catch (error) {
-    console.log("Não foi possível resgatar foto de animal.");
-  }
-
-  return (
-    <View>
-      <ListItem
-        key={pet.uid}
-        bottomDivider
-        onPress={() => {
-          navigation.navigate('Pet', pet);
-        }}>
-        <LargeImage source={petPhoto.uri} />
-        <ListItem.Content>
-          <ListItem.Title>{pet.petName}</ListItem.Title>
-        </ListItem.Content>
-      </ListItem>
-    </View>
-  );
-};
+import {Alert} from 'react-native';
 
 const PetCard = ({navigation, pet}) => {
+  const [petPhoto, setPetPhoto] = useState({
+    uri:
+      'https://i.pinimg.com/originals/18/82/e0/1882e07aecdf7a3286a5013cdad5d0c0.png',
+  });
 
-  const [petPhoto, setPetPhoto] = useState({uri: "https://i.pinimg.com/originals/18/82/e0/1882e07aecdf7a3286a5013cdad5d0c0.png"});
+  const loadPhoto = async () => {
+    try {
+      const image = storage().ref().child(pet.photoFile);
+      // const image = images.child('image1');
+      image
+        .getDownloadURL()
+        .then((url) => {
+          setPetPhoto({uri: url});
+        })
+        .catch(() => {
+          console.log('Não foi possível resgatar foto do interessado.');
+        });
+    } catch (error) {
+      console.log('Não foi possível resgatar foto do interessado.');
+    }
+  };
 
-  try {
-    const image = storage().ref().child(pet.photoFile);
-    // const image = images.child('image1');
-    image.getDownloadURL().then((url) => { 
-      setPetPhoto({ uri: url })
-      // console.log(petPhoto);
-    })
-    .catch(error => {
-      console.log("Não foi possível resgatar foto de animal.");
-    });
-  } catch (error) {
-    console.log("Não foi possível resgatar foto de animal.");
-  }
+  useEffect(() => {
+    loadPhoto();
+  }, []);
 
   return (
     <Card
@@ -74,11 +46,15 @@ const PetCard = ({navigation, pet}) => {
         titleStyle={styles.titleInfo}
       />
       <Card.Cover source={{uri: petPhoto.uri}} />
-      <Button labelStyle={styles.bottomInfo} onPress={() => {
-        console.log(pet.intentios);
-        if((pet.intentios != null) && (pet.intentios.length > 0))  navigation.navigate('Interessados', pet);
-        else Alert.alert("Não há interessados na adoção ainda.");
-      }}>Interessados</Button>
+      <Button
+        labelStyle={styles.bottomInfo}
+        onPress={() => {
+          if (pet.intentios != null && pet.intentios.length > 0)
+            navigation.navigate('Interessados', pet);
+          else Alert.alert('Não há interessados na adoção ainda.');
+        }}>
+        Interessados
+      </Button>
       {/* <Button labelStyle={styles.bottomInfo}>Apadrinhamento | Ajuda</Button> */}
     </Card>
   );
@@ -94,30 +70,13 @@ export default function MeusPets({navigation}) {
     setPets(data);
   };
 
-  // useEffect(() => {
-  //   let isCancelled = false;
-  //   getAll(user.uid).then((animals) => {
-  //     animals.forEach((animal) => {
-  //       if (!isCancelled) {
-  //         setPets((oldPets) => [...oldPets, {...animal.data(), id: animal.id}]);
-  //       }
-  //     });
-  //   });
-  //   return () => {
-  //     isCancelled = true;
-  //   };
-  // }, []);
-
   useEffect(() => {
     loadData();
   }, []);
 
-
   return (
     <View>
-      <Text>Meus Pets Page</Text>
       {pets.map((pet) => (
-        // <PetItem navigation={navigation} pet={pet} />
         <View key={pet.id}>
           <PetCard navigation={navigation} pet={{...pet.data(), id: pet.id}} />
         </View>
