bff8e14e2bc96b9bfc39b1fc864e4a3550e9a61c
Cristiane Naves <cristianenavescardoso09@gmail.com>
2021-05-16 21:58:28 -0300

    arrumando menu

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
 import React, {useContext} from 'react';
 import {View} from 'react-native';
-import {LargeButton} from '../../components/Button';
 import AuthContext from '../../contexts/auth';
+import {Button} from 'react-native-elements';
 
 export default function Menu( {navigation} ) {
   const { signed } = useContext(AuthContext);
@@ -11,16 +11,63 @@ export default function Menu( {navigation} ) {
 
   return (
     <View>
-      <LargeButton title="Meu Perfil" onPress={() => navigation.navigate('Perfil')} />
-      <LargeButton title="Meus Animais" onPress={() => navigation.navigate('MeusPets')} />
+      <View style={{alignItems: 'center', marginTop: 20}}>
+        <Button
+          titleStyle={{color: '#434343', fontFamily: 'Roboto Regular', textTransform: 'uppercase'}}
+          buttonStyle={{width: 232, height: 50, backgroundColor: '#88c9bf'}}
+          title="Meu Perfil"
+          onPress={() => navigation.navigate('Perfil')}
+        />
+      </View>
+      <View style={{alignItems: 'center', marginTop: 20}}>
+        <Button
+          titleStyle={{color: '#434343', fontFamily: 'Roboto Regular', textTransform: 'uppercase'}}
+          buttonStyle={{width: 232, height: 50, backgroundColor: '#88c9bf'}}
+          title="Meus Pets"
+          onPress={() => navigation.navigate('MeusPets')}
+        />
+      </View>
+      <View style={{alignItems: 'center', marginTop: 20}}>
+        <Button
+          titleStyle={{color: '#434343', fontFamily: 'Roboto Regular', textTransform: 'uppercase'}}
+          buttonStyle={{width: 232, height: 50, backgroundColor: '#88c9bf'}}
+          title="Cadastrar Animal"
+          onPress={() => navigation.navigate('CadastroPet')}
+        />
+      </View>
+      <View style={{alignItems: 'center', marginTop: 20}}>
+        <Button
+          titleStyle={{color: '#434343', fontFamily: 'Roboto Regular', textTransform: 'uppercase'}}
+          buttonStyle={{width: 232, height: 50, backgroundColor: '#88c9bf'}}
+          title="Adotar"
+          onPress={() => navigation.navigate('Adotar')}
+        />
+      </View>
+      <View style={{alignItems: 'center', marginTop: 20}}>
+        <Button
+          titleStyle={{color: '#434343', fontFamily: 'Roboto Regular', textTransform: 'uppercase'}}
+          buttonStyle={{width: 232, height: 50, backgroundColor: '#88c9bf'}}
+          title="Notificações"
+          onPress={() => navigation.navigate('Notificacoes')}
+        />
+      </View>
+      <View style={{alignItems: 'center', marginTop: 20}}>
+        <Button
+          titleStyle={{color: '#434343', fontFamily: 'Roboto Regular', textTransform: 'uppercase'}}
+          buttonStyle={{width: 232, height: 50, backgroundColor: '#88c9bf'}}
+          title="Chats"
+          onPress={() => navigation.navigate('Chats')}
+        />
+      </View>
+      <View style={{marginTop: 44, marginBottom: 68}}>
+        <Button
+          title="logout"
+          type="clear"
+          titleStyle={{color: '#88c9bf', fontSize: 16}}
+          onPress={() => logOut()}
+        />
+      </View>
 
-      <LargeButton title="Cadastrar Animal" onPress={() => navigation.navigate('CadastroPet')} />
-      <LargeButton title="Adotar" onPress={() => navigation.navigate('Adotar')} />
-
-      <LargeButton title="Notificações" onPress={() => navigation.navigate('Notificacoes')}/>
-      <LargeButton title="Chats" onPress={() => navigation.navigate('Chats')}/>
-
-      <LargeButton title="Logout" onPress={() => logOut()} />
     </View>
   );
 }
